3f07accda74998a80b1ac46087102123ac8f17e0
Sabir <12734@holbertonstudents.com>
2026-03-28 00:08:20 +0400

    Added some indentations

--- i18n/1-app.py
+++ i18n/1-app.py
@@ -8,6 +8,7 @@ from flask_babel import Babel
 app = Flask(__name__)
 babel = Babel(app)
 
+
 class Config:
     """
     We use that class for create basic config parameters for our app
@@ -15,6 +16,7 @@ class Config:
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
+
 
 app.config.from_object(Config)
 
