140ff99b6fe10d6bf4e1417d1382b6b91a22aef2
Sabir <12734@holbertonstudents.com>
2026-03-28 00:06:52 +0400

    Created files for task 1

---
+++ i18n/1-app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+THis file we use for create a simple Flask App
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+babel = Babel(app)
+
+class Config:
+    """
+    We use that class for create basic config parameters for our app
+    """
+    languages = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+app.config.from_object(Config)
+
+
+@app.route('/')
+def main_page():
+    """
+    This route is main route, which show us the main page
+    """
+    return render_template('0-index.html')
+
+
+if "__main__" == __name__:
+    app.run(debug=True)
