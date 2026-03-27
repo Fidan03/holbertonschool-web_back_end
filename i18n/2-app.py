da2d32a7343a95e20c79ceed097c4d2231bc740f
Sabir <12734@holbertonstudents.com>
2026-03-28 00:18:29 +0400

    Some cosmetics fixes

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -28,15 +28,15 @@ def get_locale():
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
+babel.init_app(app, locale_selector=get_locale())
+
+
 @app.route('/')
 def main_page():
     """
     This route is main route, which show us the main page
     """
     return render_template('2-index.html')
-
-
-babel.init_app(app, locale_selector=get_locale())
 
 
 if __name__ == "__main__":
